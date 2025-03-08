1d129792335207008c957e9641f509c50cf62ef0
Gilmar Rodrigues Campelo <gilmarcampelo@gmail.com>
2025-03-07 23:05:24 -0300

    Adicionar controller Produto

---
+++ ComprasWeb/Data/ComprasWebContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ComprasWeb.Models;
+
+    public class ComprasWebContext : DbContext
+    {
+        public ComprasWebContext (DbContextOptions<ComprasWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ComprasWeb.Models.Produto> Produto { get; set; } = default!;
+    }
---
+++ ComprasWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComprasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
